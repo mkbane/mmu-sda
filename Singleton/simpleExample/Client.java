7ccb0f6dd6bbf899763644e19ea2b6fe0ed7bd61
Michael K Bane <90398937+mkbane@users.noreply.github.com>
2023-01-26 10:01:54 +0000

    populate Client to call uniques

--- Singleton/simpleExample/Client.java
+++ Singleton/simpleExample/Client.java
@@ -1,5 +1,19 @@
 public class Client {
-public static void main(String[] args) {
-System.out.println("Client begins...");
-}
+    public static void main(String[] args) {
+        int NUM = 10;
+        System.out.printf("Client will call 'Unique' %d times...\n", NUM);
+        for (int i=0; i<NUM; i++) {
+            Unique U = new Unique();
+        }
+
+        /* this will not compile since private constructor
+        System.out.printf("Attempt to call 'UniqueSingleton' constructor\n");
+        UniqueSingleton US = new UniqueSingleton();
+        */
+
+        System.out.printf("Client will call 'UniqueSingleton' %d times...\n", NUM);
+        for (int i=0; i<NUM; i++) {
+            UniqueSingleton US = UniqueSingleton.getInstance();
+        }
+    }
 }
